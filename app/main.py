5eb13cd72cef3d4b8005697d6d474bc908921f95
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-27 09:52:05 +0700

    Update main.py

--- app/main.py
+++ app/main.py
@@ -3,22 +3,8 @@ from mangum import Mangum
 
 app = FastAPI()
 
-@app.get("/")
-def read_root():
-    return {"Hello": "World"}
+@app.get("/mothly/{money}")
+def get_money(money: int):
 
-@app.get("/grade/{score}")
-def get_grade(score: int):
-    if score >= 80:
-        grade = "A"
-    elif score >= 70:
-        grade = "B"
-    elif score >= 60:
-        grade = "C"
-    elif score >= 50:
-        grade = "D"
-    else:
-        grade = "F"
-    return {"grade": grade}
-
+    
 handler = Mangum(app)
