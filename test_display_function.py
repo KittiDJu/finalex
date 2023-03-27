c3574832349b0726c252476ca2e12a8362e45474
Kittiphon Julapak <111670386+KittiDJu@users.noreply.github.com>
2023-03-27 11:53:30 +0700

    Update test_display_function.py

--- test_display_function.py
+++ test_display_function.py
@@ -3,9 +3,9 @@ import pytest
 
 @pytest.mark.display
 @pytest.mark.parametrize('input_number,expected_result', [ 
-    (150000,500), (150000.50,500.001666667), (0,"Error: Price must be a positive non-zero number"), (-1,"Error: Price must be a positive non-zero number"),
-    (-1.5,"Error: Price must be a positive non-zero number"), ("a","Error: Please input integer or float price"), 
-    ("&","Error: Please input integer or float price")
+    (150000,500), (150000.50,500.001666667), (0,"Error: Error: Please input positive integer or float price"), (-1,"Error: Price must be a positive non-zero number"),
+    (-1.5,"Error: Price must be a positive non-zero number"), ("a","Error: Please input positive integer or float price"), 
+    ("&","Error: Please input positive integer or float price")
 ])
 def test_display(input_number, expected_result):
     actual_result =  display_monthly(input_number)
